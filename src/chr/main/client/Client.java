e29f9b0617e8faf1e5fb96244432ffac65dcb7b0
crisXand05 <cristianmndz009@gmail.com>
2023-02-14 17:01:44 -0600

    Main class client

---
+++ src/chr/main/client/Client.java
@@ -0,0 +1,18 @@
+package chr.main.client;
+
+import javax.swing.*;
+
+
+public class Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		FrameClient mimarco=new FrameClient();
+		
+		mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+
+}
+
