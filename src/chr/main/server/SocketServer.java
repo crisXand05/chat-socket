70917cc3a011fc9d8d899bb95a0bccb4737e5fa7
crisXand05 <cristianmndz009@gmail.com>
2023-02-14 17:16:42 -0600

    socket server main class

---
+++ src/chr/main/server/SocketServer.java
@@ -0,0 +1,23 @@
+package chr.main.server;
+
+import javax.swing.*;
+
+import chr.model.client.*;
+
+import java.awt.*;
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+
+public class SocketServer  {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		FrameServer mimarco=new FrameServer();
+		
+		mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			
+	}	
+}
+
